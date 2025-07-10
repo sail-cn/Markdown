5e923a429181244d4cbae600bb8de9415236080c
Yanxi Feng <yvan.feng@grabtaxi.com>
2025-07-10 18:46:49 +0800

    Initial Commit

---
+++ Markdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Markdown
+//
+//  Created by Yanxi Feng on 2025/7/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Markdown/MarkdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarkdownApp.swift
+//  Markdown
+//
+//  Created by Yanxi Feng on 2025/7/10.
+//
+
+import SwiftUI
+
+@main
+struct MarkdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
